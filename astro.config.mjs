f340f33cdd31d35bbe87fe8b45b4025cb50d4870
Parajulibkrm <bikramparajuli0000@gmail.com>
2023-01-15 11:31:51 +0545

    feat: :heavy_plus_sign: Added Sitemap Plugin for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs(), sitemap()]
 });
